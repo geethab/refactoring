778983cf8e08ab6d45a985c3393868ea12aedf2b
geethab <geetha.balasundaram@gmail.com>
2012-11-15 00:26:49 +0530

    Geetha - Added dummy main class needed for VS

---
+++ MainClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace refactoring
+{
+    class MainClass
+    {
+        static void Main(String [] args)
+        {
+            
+        }
+    }
+}
